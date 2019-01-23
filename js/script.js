05afa59182fd1e717d30c8775b22ace3e7507ae6
Aaron Taylor and Sam Stoia <sam@>
2019-01-23 10:04:31 -0800

    Create a JS for loop to make a word puzzle

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+  var phrase = "One small step for man, one giant leap for mankind."
+  var letters = ""
+
+  for (index = 0; index < phrase.length; index += 1) {
+      var letter = phrase[index].replace(/a|e|i|o|u/gi, "-");
+      letters += letter
+
+    }
+
+    $("#puzzle").text(letters);
+
+
+});
